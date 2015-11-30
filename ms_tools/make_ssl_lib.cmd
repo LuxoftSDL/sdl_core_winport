33fc25606b11b207a5cc9fa56db58ca54fddad73
AGaliuzov <AGaliuzov@luxoft.com>
2015-11-30 08:48:59 +0200

    Change ssl script in order to build win32 version
    Add ability to automaticaly figure out what kind of OpenSSL
    build is required.
    In case user configure building for Qt port OpenSSL will be build as 32bit
    otherwise as 64bit.

--- ms_tools/make_ssl_lib.cmd
+++ ms_tools/make_ssl_lib.cmd
@@ -1,21 +1,52 @@
-@echo off 
-IF NOT EXIST "%BUILDDIR%\openssl" (
-pushd %BUILDDIR% > NUL
-@echo "Clone OpenSSSl"
+@echo off
+
+IF "%1"=="" GOTO :usage
+
+set build_type=%1
+
+IF NOT EXIST %BUILDDIR%\openssl (
+@echo "Clone OpenSSL"
 git.exe clone https://github.com/openssl/openssl.git
-pop > NUL
 )
-IF NOT EXIST "C:\Build-OpenSSL-VC-64" (
+
+IF %build_type%==x86 (
+set CONFIG_OPT=VC-WIN32 no-asm
+set ENV="%ProgramW6432%\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
+) ELSE IF %build_type%==x64 (
+set CONFIG_OPT=VC-WIN64A
+set ENV="%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat" x86_amd64
+) ELSE GOTO :usage
+
+IF NOT EXIST %BUILDDIR%\openssl_build_%build_type% (
 pushd %BUILDDIR%\openssl > NUL
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat" x86_amd64
-git.exe checkout origin/OpenSSL_1_0_2-stable
-@echo "Build OpenSSSl for VC x64"
-perl.exe Configure VC-WIN64A --prefix=C:\Build-OpenSSL-VC-64
+
+call git.exe clean -fxd
+call git.exe checkout origin/OpenSSL_1_0_2-stable
+
+@echo "Build OpenSSl for VC %build_type%"
+
+call %ENV%
+perl.exe Configure %CONFIG_OPT% --prefix=%BUILDDIR%\openssl_build_%build_type%
+
+IF %build_type%==x86 (
+call ms\do_ms
+nmake -f ms\ntdll.mak
+nmake -f ms\ntdll.mak install
+)ELSE if %build_type%==x64(
 call ms\do_win64a
 nmake -f ms\nt.mak
 nmake -f ms\nt.mak install
-setx OPENSSL_CONF C:\Build-OpenSSL-VC-64\bin\openssl.cfg /M
-setx OPENSSL_DIR C:\\Build-OpenSSL-VC-64 /M
-pop > NUL
+) ELSE goto :usage
+popd > NUL
 )
+
+IF NOT $OPENSSL_DIR==%BUILDDIR%\openssl_build_%build_type% (
+setx  OPENSSL_CONF %BUILDDIR%\openssl_build_%build_type%\bin\openssl.cfg
+setx  OPENSSL_DIR %BUILDDIR%\openssl_build_%build_type% 
+)
+
+goto :endl
+
+:usage
+@echo "please specify either x86 or x64 build type"
 :endl
