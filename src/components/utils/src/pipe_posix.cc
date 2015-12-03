83efe55a85d25e5813c04be5f2acd311572b50bd
Nosach Artem <ANosach@luxoft.com>
2015-12-03 11:40:24 +0200

    Create crossplatform pipe wrapper

---
+++ src/components/utils/src/pipe_posix.cc
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2015, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <windows.h>
+#include <string>
+
+#include "utils/pipe.h"
+
+namespace utils {
+
+class Pipe::Impl {
+ public:
+  Impl();
+  explicit Impl(int pipe);
+  ~Impl();
+
+  Impl(Impl& rh);
+  Impl& operator=(Impl& rh);
+
+  bool Valid() const;
+
+  bool Create(const std::string& name);
+
+  bool Open();
+  bool Close();
+
+  size_t Write(const char* buf, size_t length);
+
+ private:
+  int         pipe_;
+  std::string name_;
+};
+
+} // namespace utils
+
+utils::Pipe::Pipe()
+  : impl_(new Pipe::Impl()) {
+}
+
+utils::Pipe::~Pipe() {
+  delete impl_;
+}
+
+utils::Pipe::Pipe(Pipe& rh) {
+  impl_ = new Pipe::Impl();
+  *impl_ = *rh.impl_;
+}
+
+utils::Pipe& utils::Pipe::operator=(Pipe& rh) {
+  if (this != &rh) {
+    Pipe tmp(rh);
+    this->Swap(tmp);
+  }
+  return *this;
+}
+
+bool utils::Pipe::Valid() const {
+  return impl_->Valid();
+}
+
+bool utils::Pipe::Create(const std::string& name) {
+  return impl_->Create(name);
+}
+
+bool utils::Pipe::Open() {
+  return impl_->Open();
+}
+
+bool utils::Pipe::Close() {
+  return impl_->Close();
+}
+
+size_t utils::Pipe::Write(const char* buf, size_t length) {
+  return impl_->Write(buf, length);
+}
+
+utils::Pipe::Pipe(Pipe::Impl* impl)
+  : impl_(impl) {
+}
+
+void utils::Pipe::Swap(Pipe& rh) {
+  Pipe::Impl* tmp = this->impl_;
+  this->impl_ = rh.impl_;
+  rh.impl_ = tmp;
+}
+
+utils::Pipe::Impl::Impl(): pipe_(NULL) {
+}
+
+utils::Pipe::Impl::Impl(HANDLE pipe)
+  : pipe_(INVALID_HANDLE_VALUE == pipe ? NULL : pipe) {
+}
+
+utils::Pipe::Impl::~Impl() {
+  Close();
+}
+
+utils::Pipe::Impl::Impl(Impl& rh)
+  : pipe_(rh.pipe_) {
+  rh.pipe_ = NULL;
+  rh.name_ "";
+}
+
+utils::Pipe::Impl& utils::Pipe::Impl::operator=(Impl& rh) {
+  Close();
+  pipe_ = rh.pipe_;
+  rh.pipe_ = NULL;
+  rh.name_ "";
+  return *this;
+}
+
+bool utils::Pipe::Impl::Valid() const {
+  return pipe_ != NULL;
+}
+
+bool utils::Pipe::Impl::Create(const std::string& name) {
+  if (!Close()) {
+    return false;
+  }
+  pipe_ = mkfifo(name.c_str(),
+                 S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+  if (INVALID_HANDLE_VALUE == pipe_) {
+    pipe_ = NULL;
+    return false;
+  }
+  name_ = name;
+  return true;
+}
+
+bool utils::Pipe::Impl::Open() {
+  if (NULL == pipe_ || -1 == open(name_, O_RDWR, 0)) {
+    return false;
+  }
+  return true;
+}
+
+bool utils::Pipe::Impl::Close() {
+  if (NULL == pipe_) {
+    return true;
+  }
+  if (-1 == close(pipe_) || -1 == unlink(name_)) {
+    return false;
+  }
+  pipe_ = NULL;
+  name_ = "";
+  return true;
+}
+
+size_t utils::Pipe::Impl::Write(const char* buf, size_t length) {
+  if (NULL == pipe_) {
+    return -1;
+  }
+  return write(pipe_, buf, length);
+}
