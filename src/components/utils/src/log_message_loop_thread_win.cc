cf486db2f76a35970850f064b03635d5f2667a17
Rodion Malinovsky <rodionmalino@gmail.com>
2015-12-07 12:59:54 +0200

    Disbale win logger for the qt port

--- src/components/utils/src/log_message_loop_thread_win.cc
+++ src/components/utils/src/log_message_loop_thread_win.cc
@@ -29,7 +29,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#if defined(OS_WINDOWS)
+#if defined(OS_WINDOWS) && !defined(QT_PORT)
 
 #include <string>
 #include <cstdint>
