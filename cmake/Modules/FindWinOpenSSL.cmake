8fb873ea15d71cae17ce8c9c0fd3679742914115
Rodion Malinovsky <rodion.malinovsky@binary-studio.com>
2015-12-20 08:55:19 +0200

    Remove build of the openssl from the cmake
    Rely on presence of the openssl build

--- cmake/Modules/FindWinOpenSSL.cmake
+++ cmake/Modules/FindWinOpenSSL.cmake
@@ -3,13 +3,10 @@
 #  OPENSSL_INCLUDE_DIRECTORY - the OpenSSL include directory
 #  OPENSSL_LIB_DIRECTORY - the OpenSSL lib directory
 if(QT_PORT)
-  set(build_type x86)
+  set(OPENSSL_DIR ${CMAKE_SOURCE_DIR}/build/openssl_win_x86)
 else()
-  set(build_type x64)
+  set(OPENSSL_DIR ${CMAKE_SOURCE_DIR}/build/openssl_win_x64)
 endif()
-execute_process(
-  COMMAND ${CMAKE_SOURCE_DIR}/ms_tools/make_ssl_lib.cmd ${build_type}
-)
 
-set(OPENSSL_INCLUDE_DIRECTORY $ENV{OPENSSL_DIR}/include)
-set(OPENSSL_LIB_DIRECTORY $ENV{OPENSSL_DIR}/lib)
+set(OPENSSL_INCLUDE_DIRECTORY ${OPENSSL_DIR}/include)
+set(OPENSSL_LIB_DIRECTORY ${OPENSSL_DIR}/lib)
