304484800f7081382ffb0e42aea9547a76b78927
Rodion Malinovsky <rodion.malinovsky@binary-studio.com>
2015-12-20 09:47:33 +0200

    Fit typo in the var name

--- ms/cmake_generate.cmd
+++ ms/cmake_generate.cmd
@@ -8,15 +8,15 @@ pushd %SDL_BUILD_DIR%
 if %errorlevel% == 1 goto builddir_error
 
 if %GEN_TYPE% == WinQt_x86 (
-  set CMAKE_COMAMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% -DCMAKE_INSTALL_PREFIX:PATH="%SDL_QT_DIR%\5.5\msvc2010" -DQT_PORT=1 -DCMAKE_BUILD_TYPE=Debug %SDL_ROOT%
-  @echo Generating for the win Qt x86: !CMAKE_COMAMAND!
-  call !CMAKE_COMAMAND!
+  set CMAKE_COMMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% -DCMAKE_INSTALL_PREFIX:PATH="%SDL_QT_DIR%\5.5\msvc2010" -DQT_PORT=1 -DCMAKE_BUILD_TYPE=Debug %SDL_ROOT%
+  @echo Generating for the win Qt x86: !CMAKE_COMMAND!
+  call !CMAKE_COMMAND!
   goto end
 )
 if %GEN_TYPE% == WinNative_x64 (
-  set CMAKE_COMAMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% %SDL_ROOT%
-  @echo Generating for the win native x64: !CMAKE_COMAMAND!
-  call !CMAKE_COMAMAND!
+  set CMAKE_COMMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% %SDL_ROOT%
+  @echo Generating for the win native x64: !CMAKE_COMMAND!
+  call !CMAKE_COMMAND!
   goto end
 )
 goto usage
