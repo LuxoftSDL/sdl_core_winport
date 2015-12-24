f24698ce54a911ccbeb0837c98869a55eba1b153
Krotenko <OKrotenko@kiev.luxoft.com>
2015-12-24 12:52:51 +0200

    Modify common-setup.cmd for currect setup custom build dir

--- ms/common_setup.cmd
+++ ms/common_setup.cmd
@@ -1,22 +1,19 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-set _NPARAM=%~1		
-echo "%_NPARAM%"
-IF /I "%_NPARAM%"=="" goto default_build
-  set BUILD_DIR=%1
-  goto finish
-
-
-:default_build
+set CUSTOM_BUIL_DIR=%~1
+if "%CUSTOM_BUIL_DIR%"=="" (
 set BUILD_DIR="%SDL_ROOT%\build"
-:finish
-set SDL_BUILD_DIR="%BUILD_DIR%\%SDL_BUILD%"
-if not exist %SDL_BUILD_DIR% mkdir %SDL_BUILD_DIR%
-if not exist %SDL_BUILD_DIR% goto error
+) else (
+  set BUILD_DIR=%CUSTOM_BUIL_DIR%
+)
+set SDL_BUILD_DIR="!BUILD_DIR!\%SDL_BUILD%"
+if not exist !SDL_BUILD_DIR! mkdir !SDL_BUILD_DIR!
+if not exist !SDL_BUILD_DIR! goto error
 goto end
 
 :error
-@echo "Could not create build directory %SDL_BUILD_DIR%"
+@echo "Could not create build directory !SDL_BUILD_DIR!"
 exit /B 2
 
 :end
