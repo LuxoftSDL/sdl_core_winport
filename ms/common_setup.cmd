ae89ab35e318a789f83b9d1a856b8496e2727b16
Krotenko <OKrotenko@kiev.luxoft.com>
2015-12-24 10:37:13 +0200

    Add custom path to the build directory

--- ms/common_setup.cmd
+++ ms/common_setup.cmd
@@ -1,6 +1,15 @@
 @echo off
 
+set _NPARAM=%~1		
+echo "%_NPARAM%"
+IF /I "%_NPARAM%"=="" goto default_build
+  set BUILD_DIR=%1
+  goto finish
+
+
+:default_build
 set BUILD_DIR="%SDL_ROOT%\build"
+:finish
 set SDL_BUILD_DIR="%BUILD_DIR%\%SDL_BUILD%"
 if not exist %SDL_BUILD_DIR% mkdir %SDL_BUILD_DIR%
 if not exist %SDL_BUILD_DIR% goto error
--- setup_VS2013_x64.cmd
+++ setup_VS2013_x64.cmd
@@ -6,7 +6,7 @@ set SDL_ROOT=%CD%
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat" x86_amd64
 
-call "%SDL_ROOT%\ms\common_setup.cmd"
+call "%SDL_ROOT%\ms\common_setup.cmd" %*
 if %errorlevel% == 2 exit /b 1
 
 call "%SDL_ROOT%\ms\openssl_setup.cmd" x64
