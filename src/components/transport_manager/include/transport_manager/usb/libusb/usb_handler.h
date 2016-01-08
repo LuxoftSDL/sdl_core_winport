f6e84230702fb239e7c7bf3014a23ac17b89b1a7
Nosach Artem <ANosach@luxoft.com>
2016-01-08 14:48:27 +0200

    Override exitThreadMain() method in UsbHandler

--- src/components/transport_manager/include/transport_manager/usb/libusb/usb_handler.h
+++ src/components/transport_manager/include/transport_manager/usb/libusb/usb_handler.h
@@ -46,6 +46,7 @@
 #include "transport_manager/usb/libusb/platform_usb_device.h"
 #include "transport_manager/usb/usb_control_transfer.h"
 #include "utils/threads/thread.h"
+#include "utils/atomic_object.h"
 
 class Thread;
 
@@ -80,12 +81,13 @@ class UsbHandler {
    public:
     explicit UsbHandlerDelegate(UsbHandler* handler);
     void threadMain() OVERRIDE;
+    void exitThreadMain() OVERRIDE;
 
    private:
     UsbHandler* handler_;
   };
 
-  bool shutdown_requested_;
+  sync_primitives::atomic_bool shutdown_requested_;
   threads::Thread* thread_;
 
   friend class UsbDeviceListener;
