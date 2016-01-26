e2910bd9be3ebc78c73bb6ba42f8e48f34341dd7
Rodion Malinovsky <rodionmalino@gmail.com>
2016-01-26 18:23:50 +0200

    Update the server socket usage in the socket_streamer_adapter

--- src/components/media_manager/src/socket_streamer_adapter.cc
+++ src/components/media_manager/src/socket_streamer_adapter.cc
@@ -29,18 +29,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#if defined(OS_WINDOWS)
-#include "utils/winhdr.h"
-
-#elif defined(OS_POSIX)
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#endif
-
-#include "utils/logger.h"
-#include "utils/socket.h"
 #include "media_manager/socket_streamer_adapter.h"
+#include "utils/logger.h"
 
 namespace media_manager {
 
@@ -72,7 +62,7 @@ bool SocketStreamerAdapter::SocketStreamer::Connect() {
   LOG4CXX_AUTO_TRACE(logger);
 
   const int backlog = 5;
-  if (!server_socket_.Listen(ip_, port_, backlog)) {
+  if (!server_socket_.Listen(utils::HostAddress(ip_), port_, backlog)) {
     LOG4CXX_ERROR(logger, "Unable to listen");
     return false;
   }
@@ -97,24 +87,23 @@ void SocketStreamerAdapter::SocketStreamer::Disconnect() {
 bool SocketStreamerAdapter::SocketStreamer::Send(
     protocol_handler::RawMessagePtr msg) {
   LOG4CXX_AUTO_TRACE(logger);
-  size_t ret;
+  std::size_t written = 0u;
   if (is_first_frame_) {
-    ret = client_socket_.Send(header_.c_str(), header_.size());
-    if (ret != header_.size()) {
+    bool sent = client_socket_.Send(header_.c_str(), header_.size(), written);
+    if (!sent || written != header_.size()) {
       LOG4CXX_ERROR(logger, "Unable to send data to socket");
       return false;
     }
     is_first_frame_ = false;
   }
 
-  ret = client_socket_.Send(reinterpret_cast<const char*>(msg->data()),
-                            msg->data_size());
-  if (-1 == ret) {
+  bool sent = client_socket_.Send(msg->data(), msg->data_size(), written);
+  if (!sent) {
     LOG4CXX_ERROR(logger, "Unable to send data to socket");
     return false;
   }
 
-  if (ret != msg->data_size()) {
+  if (written != msg->data_size()) {
     LOG4CXX_WARN(logger, "Couldn't send all the data to socket");
   }
 
