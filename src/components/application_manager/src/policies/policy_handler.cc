3e7bb235b0442f31b35c02c0949a2ddf82cd780a
Nosach Artem <ANosach@luxoft.com>
2016-02-01 17:59:18 +0200

    Pass logger reference to Policy.dll

--- src/components/application_manager/src/policies/policy_handler.cc
+++ src/components/application_manager/src/policies/policy_handler.cc
@@ -315,19 +315,16 @@ bool PolicyHandler::PolicyEnabled() {
 
 bool PolicyHandler::CreateManager() {
   typedef PolicyManager* (*CreateManager)(
-      const std::string&, uint16_t, uint16_t);
+      const std::string&, uint16_t, uint16_t, logger::Logger::Pimpl&);
   DCHECK_OR_RETURN(policy_library_.IsLoaded(), false);
   CreateManager create_manager = reinterpret_cast<CreateManager>(
       policy_library_.GetSymbol("CreateManager"));
   if (create_manager) {
-#if defined(OS_POSIX)
-    policy_manager_ = create_manager();
-#elif defined(OS_WINDOWS)
     policy_manager_ = create_manager(
         profile::Profile::instance()->app_storage_folder(),
         profile::Profile::instance()->attempts_to_open_policy_db(),
-        profile::Profile::instance()->open_attempt_timeout_ms());
-#endif
+        profile::Profile::instance()->open_attempt_timeout_ms(),
+        GET_LOGGER());
   } else {
     LOG4CXX_WARN(logger_,
                  "Policy library loading error. Cannot get proc address");
--- src/components/policy/src/policy/src/policy_manager_impl.cc
+++ src/components/policy/src/policy/src/policy_manager_impl.cc
@@ -48,7 +48,9 @@
 
 policy::PolicyManager* CreateManager(const std::string& app_storage_folder,
                                      uint16_t attempts_to_open_policy_db,
-                                     uint16_t open_attempt_timeout_ms) {
+                                     uint16_t open_attempt_timeout_ms,
+                                     logger::Logger::Pimpl& logger) {
+  SET_LOGGER(logger);
   return new policy::PolicyManagerImpl(
       app_storage_folder, attempts_to_open_policy_db, open_attempt_timeout_ms);
 }
